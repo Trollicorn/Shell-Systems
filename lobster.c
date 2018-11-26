72ed96adff1f50e9614db11aa423d82f7dd78e6c
Trollicorn <muddin3@stuy.edu>
2018-11-26 16:31:12 -0500

    program loops and forks to run command

--- lobster.c
+++ lobster.c
@@ -6,24 +6,35 @@
 #include "pargs.h"
 
 int lobster(){
-  return 0;
-}
-
-int main(int argc, char * argv[]) {
   char m[100];// = "ls";
+//  printf("%ld\n\n",sizeof(m) );
   fgets(m, sizeof(m), stdin);
-
-  printf("%ld\n",strlen(m) );
+//  printf("%ld\n",strlen(m) );
   if(strlen(m)){
     m[strlen(m)-1] = NULL;
+  }else{
+    return;
   }
 //  printf("[%s]",m);
-  char **args = pargs(m);
-  int i = 0;
+  int f = fork();
+  if (!f){
+    char **args = pargs(m);
+/*  int i = 0;
   while(args[i]){
     printf("[%s]\n", args[i]);
     ++i;
   }
-  execvp(args[0],args);
+*/
+    execvp(args[0],args);
+  }
+  int *n;
+  wait(n);
+  return 0;
+}
+
+int main(int argc, char * argv[]) {
+  while (1) {
+    lobster();
+  }
   return 0;
 }
